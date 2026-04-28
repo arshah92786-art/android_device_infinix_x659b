86a720670f2061bf19ae7edc9f2caa21d7e4ff7c
arshah92786-art <arshah92786@gmail.com>
2026-04-28 14:05:15 +0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X659B
+DEVICE_PATH := device/infinix/x659b
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
