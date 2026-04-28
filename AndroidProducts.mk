be8b0e66aeaaa385b644d99221223987f4570c05
arshah92786-art <arshah92786@gmail.com>
2026-04-28 13:13:09 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X659B.mk
+    $(LOCAL_DIR)/omni_x659b.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X659B-user \
-    omni_Infinix-X659B-userdebug \
-    omni_Infinix-X659B-eng
+    omni_x659b-userdebug \
+    omni_x659b-eng
